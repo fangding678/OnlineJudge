4097e70be6b61cb29e12036fba48b99dc46b6753
fangding <fangding@bytedance.com>
2020-12-08 10:35:28 +0800

    fix

--- leetcode/main.cc
+++ leetcode/main.cc
@@ -10,7 +10,7 @@ using namespace std;
 #include "exercise/_c0213.h"
 
 
-int main()
+int main(int argc,char *argv[])
 {
     cout << "begin------------" << endl;
     func();
--- leetcode/run.sh
+++ leetcode/run.sh
@@ -18,16 +18,16 @@ echo "run main begin..."
 echo "run main end..."
 echo "----------------------------------------------------------------------------------------------------"
 
-function func1()
+function batch_process()
 {
     cd ..
-    for elem in exercise/*
+    for elem in archive/*
     do
-        elem1=${elem:0:11}0${elem:11}
+        elem1=${elem:0:9}c${elem:10}
         elem2=${elem1/cpp/h}
         echo "$elem2"
 #        mv ${elem} ${elem2}
     #    sed 's/main/func/g' t_archive/$elem > t_archive/_$elem
     done
 }
-func1
+
