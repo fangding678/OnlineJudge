81caf40ac84734102ec9acdbb82932a89f7bc99a
fangding <fangding@bytedance.com>
2021-02-13 09:47:44 +0800

    _299

--- leetcode/exercise/_c0297.h
+++ leetcode/exercise/_c0297.h
@@ -7,36 +7,49 @@
 #include "__debug"
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Codec {
 public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-
+        if (!root) {
+            return "#_";
+        }
+        string res = to_string(root->val) + "_";
+        res += serialize(root->left);
+        res += serialize(root->right);
+        return res;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-
+        stringstream ss(data);
+        string node;
+        queue<string> qu;
+        while (getline(ss, node, '_')) {
+            qu.push(node);
+        }
+        return helper(qu);
+    }
+    TreeNode* helper(queue<string> &qu) {
+        string val = qu.front();
+        qu.pop();
+        if (val == "#") {
+            return NULL;
+        }
+        TreeNode *t = new TreeNode(stoi(val));
+        t->left = helper(qu);
+        t->right = helper(qu);
+        return t;
     }
 
 };
 
-// Your Codec object will be instantiated and called as such:
-// Codec ser, deser;
-// TreeNode* ans = deser.deserialize(ser.serialize(root));
 
 void func() {
-    string str = "ads";
-    cout << str << endl;
+    Codec codec;
+    string data = "1_2_3_#_#_4_5_#_#_#_#_";
+    TreeNode *t = codec.deserialize(data);
+    cout << t->val << endl;
 }
 
--- leetcode/exercise/_c0299.h
+++ leetcode/exercise/_c0299.h
@@ -8,11 +8,37 @@
 using namespace std;
 
 class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls = 0, cows = 0;
+        map<char, int> m;
+        for (int i = 0; i < secret.size(); ++i) {
+            if (secret[i] == guess[i]) {
+                bulls += 1;
+            } else if (m.find(secret[i]) == m.end()) {
+                m[secret[i]] = 1;
+            } else {
+                m[secret[i]] += 1;
+            }
+        }
+        for (int i = 0; i < secret.size(); ++i) {
+            if (secret[i] == guess[i]) {
+                continue;
+            }
+            if (m.find(guess[i]) != m.end() && m[guess[i]] > 0) {
+                cows += 1;
+                m[guess[i]]--;
+            }
+        }
+        return to_string(bulls) + "A" + to_string(cows) + "B";
+    }
 };
 
 void func() {
     Solution solution;
-    string str = "ads";
-    cout << str << endl;
+    cout << solution.getHint("1807", "7810") << endl;
+    cout << solution.getHint("1123", "0111") << endl;
+    cout << solution.getHint("11", "10") << endl;
+    cout << solution.getHint("1122", "1222") << endl;
 }
 
