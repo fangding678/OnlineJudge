1ace8df42bad461dbf38cf402fc9737965ffcb8e
fangding <fangding@bytedance.com>
2021-03-27 21:49:54 +0800

    _c0306

--- leetcode/exercise/_c0306.h
+++ leetcode/exercise/_c0306.h
@@ -16,29 +16,23 @@ public:
         stringstream ss;
         while (i1 >= 0 && i2 >= 0) {
             int ti = int(s1[i1] - '0') + int(s2[i2] - '0') + flag;
-            if (ti > 9) {
-                flag = ti / 10;
-                ti = ti % 10;
-            }
+            flag = ti / 10;
+            ti = ti % 10;
             ss << ti;
             --i1;
             --i2;
         }
         while (i1 >= 0) {
             int ti = int(s1[i1] - '0') + flag;
-            if (ti > 9) {
-                flag = ti / 10;
-                ti = ti % 10;
-            }
+            flag = ti / 10;
+            ti = ti % 10;
             ss << ti;
             --i1;
         }
         while (i2 >= 0) {
             int ti = int(s2[i2] - '0') + flag;
-            if (ti > 9) {
-                flag = ti / 10;
-                ti = ti % 10;
-            }
+            flag = ti / 10;
+            ti = ti % 10;
             ss << ti;
             --i2;
         }
@@ -46,21 +40,59 @@ public:
             ss << flag;
         }
         string s = ss.str();
-        cout << s << endl;
         reverse(s.begin(), s.end());
+//        cout << s << endl;
         return s;
+    }
+    bool isAdditiveHelper(string num, int e1, int e2) {
+        string num1 = num.substr(0, e1);
+        string num2 = num.substr(e1, e2-e1);
+        num = num.substr(e2);
+        while (num.size() > 0) {
+            string addNum = addTwoNum(num1, num2);
+            if (e1 == 4 && e2 == 8) {
+                cout << num1 << "\n" << num2 << endl;
+                cout << addNum << endl;
+            }
+            if (addNum.size() > num.size()) {
+                return false;
+            }
+            num1 = num2;
+            num2 = num.substr(0, addNum.size());
+            if (num2 != addNum) {
+                return false;
+            }
+            num = num.substr(addNum.size());
+        }
+        return true;
     }
     bool isAdditiveNumber(string num) {
         int len = num.size();
-        for (int i = 0; i < num.size(); ++i) {
-            ;
+        for (int i = 1; i < len - 1; ++i) {
+            for (int j = i+1; j < len; ++j) {
+                if (num[i] == '0') {
+                    if (isAdditiveHelper(num, i, i+1)) {
+                        return true;
+                    }
+                    break;
+                }
+                if (isAdditiveHelper(num, i, j)) {
+                    return true;
+                }
+            }
         }
-        return true;
+        return false;
     }
 };
 
 void func() {
     Solution solution;
-    cout << solution.isAdditiveNumber("112") << endl;
+    cout << solution.isAdditiveNumber("101") << endl;
+    cout << solution.isAdditiveNumber("112358") << endl;
+    cout << solution.isAdditiveNumber("199100199") << endl;
+    cout << solution.isAdditiveNumber("19910011992") << endl;
+    cout << endl;
+    cout << solution.isAdditiveNumber("198019823962") << endl;
+    cout << solution.addTwoNum("1980", "1982") << endl;
 }
 
--- leetcode/exercise/_c0307.h
+++ leetcode/exercise/_c0307.h
@@ -7,11 +7,40 @@
 #include "__debug"
 using namespace std;
 
-class Solution {
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+        sumArr.clear();
+        int t = 0;
+        sumArr.push_back(t);
+        for (auto n : nums) {
+            t += n;
+            sumArr.push_back(t);
+        }
+    }
+
+    void update(int index, int val) {
+
+    }
+
+    int sumRange(int left, int right) {
+        sumArr[right+1] - sumArr[left];
+    }
+
+private:
+    vector<int> sumArr;
 };
 
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
+
 void func() {
-    Solution solution;
+    vector<int> vec{};
+    NumArray numArray(vec);
     string str = "ads";
     cout << str << endl;
 }
